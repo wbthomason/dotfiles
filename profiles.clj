0f3c3fca151157de1e1724d43e4fe42274569d1e
Wil Thomason <wil.thomason@gmail.com>
2016-01-19 00:36:01 -0500

    Leiningen profile

---
+++ profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[cider/cider-nrepl "0.10.1"]]
+        :dependencies [[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+                       [cljfmt "0.3.0"]
+                       ]
+        }}
