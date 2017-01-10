a902596105ec0fab9dcb3d9f44c58ab9c1cbcea4
Wil Thomason <wil.thomason@gmail.com>
2017-01-10 15:17:31 -0500

    Started using Polybar - single monitor config

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar mainbar &
+
+echo "Bars launched..."
