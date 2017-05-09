3f6c2831157df8bbde8cdb3ed26dd578457e3967
Wil Thomason <wil.thomason@gmail.com>
2017-05-09 00:13:09 -0400

    Add ptpython config

---
+++ pytpython-config.py
@@ -0,0 +1,120 @@
+"""
+Configuration example for ``ptpython``.
+
+Copy this file to ~/.ptpython/config.py
+"""
+from __future__ import unicode_literals
+from prompt_toolkit.filters import ViInsertMode
+from prompt_toolkit.key_binding.input_processor import KeyPress
+from prompt_toolkit.keys import Keys
+from pygments.token import Token
+
+from ptpython.layout import CompletionVisualisation
+
+__all__ = (
+    'configure',
+)
+
+
+def configure(repl):
+    """
+    Configuration method. This is called during the start-up of ptpython.
+
+    :param repl: `PythonRepl` instance.
+    """
+    # Show function signature (bool).
+    repl.show_signature = True
+
+    # Show docstring (bool).
+    repl.show_docstring = True
+
+    # Show completions. (NONE, POP_UP, MULTI_COLUMN or TOOLBAR)
+    repl.completion_visualisation = CompletionVisualisation.POP_UP
+
+    # When CompletionVisualisation.POP_UP has been chosen, use this
+    # scroll_offset in the completion menu.
+    repl.completion_menu_scroll_offset = 0
+
+    # Show line numbers (when the input contains multiple lines.)
+    repl.show_line_numbers = True
+
+    # Show status bar.
+    repl.show_status_bar = True
+
+    # When the sidebar is visible, also show the help text.
+    repl.show_sidebar_help = True
+
+    # Highlight matching parethesis.
+    repl.highlight_matching_parenthesis = True
+
+    # Line wrapping. (Instead of horizontal scrolling.)
+    repl.wrap_lines = True
+
+    # Mouse support.
+    repl.enable_mouse_support = True
+
+    # Complete while typing. (Don't require tab before the
+    # completion menu is shown.)
+    repl.complete_while_typing = True
+
+    # Vi mode.
+    repl.vi_mode = True
+
+    # Paste mode. (When True, don't insert whitespace after new line.)
+    repl.paste_mode = False
+
+    # Don't insert a blank line after the output.
+    repl.insert_blank_line_after_output = False
+
+    # History Search.
+    # When True, going back in history will filter the history on the records
+    # starting with the current input. (Like readline.)
+    # Note: When enable, please disable the `complete_while_typing` option.
+    #       otherwise, when there is a completion available, the arrows will
+    #       browse through the available completions instead of the history.
+    repl.enable_history_search = False
+
+    # Enable auto suggestions. (Pressing right arrow will complete the input,
+    # based on the history.)
+    repl.enable_auto_suggest = False
+
+    # Enable open-in-editor. Pressing C-X C-E in emacs mode or 'v' in
+    # Vi navigation mode will open the input in the current editor.
+    repl.enable_open_in_editor = True
+
+    # Enable system prompt. Pressing meta-! will display the system prompt.
+    # Also enables Control-Z suspend.
+    repl.enable_system_bindings = True
+
+    # Ask for confirmation on exit.
+    repl.confirm_exit = False
+
+    # Enable input validation. (Don't try to execute when the input contains
+    # syntax errors.)
+    repl.enable_input_validation = True
+
+    # Enable 24bit True color. (Not all terminals support this. -- maybe check
+    # $TERM before changing.)
+    repl.true_color = True
+
+    # Add custom key binding for PDB.
+    @repl.add_key_binding(Keys.ControlB)
+    def _(event):
+        ' Pressing Control-B will insert "pdb.set_trace()" '
+        event.cli.current_buffer.insert_text('\nimport pdb; pdb.set_trace()\n')
+
+    # Typing ControlE twice should also execute the current command.
+    # (Alternative for Meta-Enter.)
+    @repl.add_key_binding(Keys.ControlE, Keys.ControlE)
+    def _(event):
+        b = event.current_buffer
+        if b.accept_action.is_returnable:
+            b.accept_action.validate_and_handle(event.cli, b)
+
+
+    # Typing 'jj' in Vi Insert mode, should send escape. (Go back to navigation
+    # mode.)
+    @repl.add_key_binding('j', 'j', filter=ViInsertMode())
+    def _(event):
+        " Map 'jj' to Escape. "
+        event.cli.input_processor.feed(KeyPress(Keys.Escape))
