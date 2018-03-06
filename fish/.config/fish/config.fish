7d814d9d0ad0b3b7c52069ebc4012c501f409882
Wil Thomason <wil.thomason@gmail.com>
2018-03-05 20:38:30 -0500

    Add fasd aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -41,6 +41,16 @@ bass source /opt/ros/lunar/setup.bash
 set -xg ROS_HOSTNAME localhost
 set -xg ROS_MASTER_URI http://localhost:11311
 
+# fasd
+alias a 'fasd -a'        # any
+alias s 'fasd -si'       # show / search / select
+alias d 'fasd -d'        # directory
+alias f 'fasd -f'        # file
+alias sd 'fasd -sid'     # interactive directory selection
+alias sf 'fasd -sif'     # interactive file selection
+alias z 'fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz 'fasd_cd -d -i' # cd with interactive selection
+
 # Tmux
 if not set -q TMUX
     exec tmux new-session -A -s sysadmin
