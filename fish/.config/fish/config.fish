fbd622fd5830fef8f278362114f147fd6853a8c1
Wil Thomason <wil.thomason@gmail.com>
2018-05-16 01:14:52 -0400

    Try using gnome-keyring instead

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,11 +23,11 @@ eval (opam config env)
 set -xg RUST_SRC_PATH '/usr/src/rust/src'
 
 # Keychain
-if status --is-interactive
-    set -l IFS
-    eval (keychain --agents ssh --eval --quiet id_rsa)
-    eval (keychain --agents gpg --eval --quiet 8BEE5C508226C4516876B93449B3E4A714642E1D)
-end
+# if status --is-interactive
+#     set -l IFS
+#     eval (keychain --agents ssh --eval --quiet id_rsa)
+#     eval (keychain --agents gpg --eval --quiet 8BEE5C508226C4516876B93449B3E4A714642E1D)
+# end
 
 # Aliases
 alias svn 'colorsvn'
