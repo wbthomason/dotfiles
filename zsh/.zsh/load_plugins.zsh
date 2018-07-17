64125611b722a8058a1acac27288a6fb4781ba6a
Wil Thomason <wil.thomason@gmail.com>
2018-07-17 16:38:32 -0400

    Tweak zsh plugins

--- zsh/.zsh/load_plugins.zsh
+++ zsh/.zsh/load_plugins.zsh
@@ -18,3 +18,7 @@ source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdha
 fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
 source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-changyuheng-SLASH-zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
 fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-changyuheng-SLASH-zsh-interactive-cd )
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
