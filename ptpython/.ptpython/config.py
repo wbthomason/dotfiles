c37c86d8e1c8acc721a3b2ef12eb63e34a20b288
Wil Thomason <wil.thomason@gmail.com>
2018-12-11 15:27:00 -0500

    Update ptpython config for new version

--- ptpython/.ptpython/config.py
+++ ptpython/.ptpython/config.py
@@ -1,11 +1,6 @@
-"""
-Configuration example for ``ptpython``.
-
-Copy this file to ~/.ptpython/config.py
-"""
 from __future__ import unicode_literals
 from prompt_toolkit.filters import ViInsertMode
-from prompt_toolkit.key_binding.input_processor import KeyPress
+from prompt_toolkit.key_binding.key_processor import KeyPress
 from prompt_toolkit.keys import Keys
 from pygments.token import Token
 
@@ -19,7 +14,6 @@ __all__ = (
 def configure(repl):
     """
     Configuration method. This is called during the start-up of ptpython.
-
     :param repl: `PythonRepl` instance.
     """
     # Show function signature (bool).
@@ -27,6 +21,10 @@ def configure(repl):
 
     # Show docstring (bool).
     repl.show_docstring = True
+
+    # Show the "[Meta+Enter] Execute" message when pressing [Enter] only
+    # inserts a newline instead of executing the code.
+    repl.show_meta_enter_message = True
 
     # Show completions. (NONE, POP_UP, MULTI_COLUMN or TOOLBAR)
     repl.completion_visualisation = CompletionVisualisation.POP_UP
@@ -63,8 +61,11 @@ def configure(repl):
     # Paste mode. (When True, don't insert whitespace after new line.)
     repl.paste_mode = False
 
+    # Use the classic prompt. (Display '>>>' instead of 'In [1]'.)
+    repl.prompt_style = 'classic'  # 'classic' or 'ipython'
+
     # Don't insert a blank line after the output.
-    repl.insert_blank_line_after_output = False
+    repl.insert_blank_line_after_output = True
 
     # History Search.
     # When True, going back in history will filter the history on the records
@@ -76,7 +77,7 @@ def configure(repl):
 
     # Enable auto suggestions. (Pressing right arrow will complete the input,
     # based on the history.)
-    repl.enable_auto_suggest = False
+    repl.enable_auto_suggest = True
 
     # Enable open-in-editor. Pressing C-X C-E in emacs mode or 'v' in
     # Vi navigation mode will open the input in the current editor.
@@ -94,30 +95,14 @@ def configure(repl):
     repl.enable_input_validation = True
 
     # Use this colorscheme for the code.
-    repl.use_code_colorscheme('vim')
+    repl.use_code_colorscheme('trac')
 
-    # Enable 24bit True color. (Not all terminals support this. -- maybe check
-    # $TERM before changing.)
-    repl.true_color = True
+    # Set color depth (keep in mind that not all terminals support true color).
 
-    # Add custom key binding for PDB.
-    @repl.add_key_binding(Keys.ControlB)
-    def _(event):
-        ' Pressing Control-B will insert "pdb.set_trace()" '
-        event.cli.current_buffer.insert_text('\nimport pdb; pdb.set_trace()\n')
+    #repl.color_depth = 'DEPTH_1_BIT'  # Monochrome.
+    #repl.color_depth = 'DEPTH_4_BIT'  # ANSI colors only.
+    # repl.color_depth = 'DEPTH_8_BIT'  # The default, 256 colors.
+    repl.color_depth = 'DEPTH_24_BIT'  # True color.
 
-    # Typing ControlE twice should also execute the current command.
-    # (Alternative for Meta-Enter.)
-    @repl.add_key_binding(Keys.ControlE, Keys.ControlE)
-    def _(event):
-        b = event.current_buffer
-        if b.accept_action.is_returnable:
-            b.accept_action.validate_and_handle(event.cli, b)
-
-
-    # Typing 'jj' in Vi Insert mode, should send escape. (Go back to navigation
-    # mode.)
-    @repl.add_key_binding('j', 'j', filter=ViInsertMode())
-    def _(event):
-        " Map 'jj' to Escape. "
-        event.cli.input_processor.feed(KeyPress(Keys.Escape))
+    # Syntax.
+    repl.enable_syntax_highlighting = True
