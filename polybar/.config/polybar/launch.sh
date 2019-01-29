85479380d02a5ed112c6a729a9e729de830ce1d0
Wil Thomason <wil.thomason@gmail.com>
2019-01-29 01:28:53 -0500

    Undo accidental overwriting of polybar files

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,8 +7,6 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar left &
-polybar center &
-polybar right &
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar top & done
 
 echo "Bars launched..."
