f6a7ebfa7ee732e58277064973be4788f694e196
Wil Thomason <wil.thomason@gmail.com>
2019-03-07 21:23:44 -0500

    Fix polybar music script

--- polybar/.config/polybar/player-mpris-tail.py
+++ polybar/.config/polybar/player-mpris-tail.py
@@ -5,7 +5,7 @@ import sys
 import subprocess
 
 import gi
-gi.require_version('Playerctl', '1.0')
+gi.require_version('Playerctl', '2.0')
 from gi.repository import Playerctl, GLib
 
 MUSIC_ICON = ''
