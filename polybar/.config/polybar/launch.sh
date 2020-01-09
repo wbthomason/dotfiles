107b56a9f01a5ba42f64d23237344d67cbc880ef
Wil Thomason <wil.thomason@gmail.com>
2020-01-09 01:33:16 -0500

    Use a new polybar theme, from https://github.com/adi1090x/polybar-themes

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
+## Add this to your wm startup file.
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar top & done
-
-echo "Bars launched..."
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config.ini main &
