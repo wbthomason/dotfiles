88068202c378446c420abdf320d4de0089339186
Wil Thomason <wil.thomason@gmail.com>
2020-01-09 20:54:32 -0500

    More polybar tweaks

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,4 +9,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar -c ~/.config/polybar/config.ini main & done
