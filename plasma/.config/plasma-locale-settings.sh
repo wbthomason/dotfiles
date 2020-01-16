fb40616235356f7a6a69912bbf338e24377ce672
Wil Thomason <wil.thomason@gmail.com>
2020-01-16 18:02:20 -0500

    Remove machine-specific plasma files

---
+++ plasma/.config/plasma-locale-settings.sh
@@ -0,0 +1,6 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=en_US.UTF-8
+export LC_TIME=C
