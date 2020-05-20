da69fb58e1a32ce6d07e484bb289c06e8da31d85
Wil Thomason <wil.thomason@gmail.com>
2020-05-20 14:28:22 -0400

    Substantially improved LSP diagnostics

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -1,5 +1,65 @@
 local nvim_lsp = require('nvim_lsp')
 local lsp_status = require('lsp-status')
+local diagnostic = require('diagnostic')
+local completion = require('completion')
+
+local severity_map = {'E', 'W', 'I', 'H'}
+
+local function diagnostics_to_items(diagnostics)
+  local items = {}
+  local grouped = setmetatable({}, {
+    __index = function(t, k)
+      local v = {}
+      rawset(t, k, v)
+      return v
+    end;
+  })
+
+  for _, d in ipairs(diagnostics) do
+    local fname = vim.uri_to_fname(d.uri)
+    local range = d.range
+    table.insert(grouped[fname], { start = range.start, item = d })
+  end
+
+  local keys = vim.tbl_keys(grouped)
+  table.sort(keys)
+  for _, fname in ipairs(keys) do
+    local rows = grouped[fname]
+    table.sort(rows, function(a, b)
+      if
+        a.start.line < b.start.line or
+        (a.start.line == b.start.line and a.start.character < b.start.character)
+      then
+        return true
+      end
+
+      return false
+    end)
+
+    for _, row in ipairs(rows) do
+      table.insert(items, {
+        filename = fname,
+        lnum = row.start.line + 1,
+        text = row.item.message,
+        ['type'] = severity_map[row.item.severity],
+        col = row.start.character + 1,
+        vcol = true
+      })
+    end
+  end
+
+  return items
+end
+
+diagnostic.diagnostics_loclist = function(local_result)
+  if local_result and local_result.diagnostics then
+    for _, v in ipairs(local_result.diagnostics) do
+      v.uri = v.uri or local_result.uri
+    end
+  end
+
+  vim.lsp.util.set_loclist(diagnostics_to_items(local_result.diagnostics))
+end
 
 local texlab_search_status = vim.tbl_add_reverse_lookup {
   Success = 0;
@@ -37,8 +97,8 @@ local function make_on_attach(config)
     end
 
     lsp_status.register_client(client.name)
-    require('diagnostic').on_attach()
-    require('completion').on_attach()
+    diagnostic.on_attach()
+    completion.on_attach()
     local opts = { noremap=true, silent=true }
     vim.api.nvim_buf_set_keymap(0, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     vim.api.nvim_buf_set_keymap(0, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
@@ -61,7 +121,6 @@ local function make_on_attach(config)
     if client.resolved_capabilities.document_highlight then
       vim.api.nvim_command('augroup lsp_aucmds')
       vim.api.nvim_command('au CursorHold <buffer> lua vim.lsp.buf.document_highlight()')
-      vim.api.nvim_command('au CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()')
       vim.api.nvim_command('au CursorMoved <buffer> lua vim.lsp.buf.clear_references()')
       vim.api.nvim_command('augroup END')
     end
@@ -106,7 +165,40 @@ local servers = {
       ['python/reportProgress'] = lsp_status.extension_callbacks.pyls_ms["python/reportProgress"],
       ['python/beginProgress'] = lsp_status.extension_callbacks.pyls_ms["python/beginProgress"],
       ['python/endProgress'] = lsp_status.extension_callbacks.pyls_ms["python/endProgress"],
-    }
+    },
+    settings = {
+      python = {
+        jediEnabled = false,
+        analysis = {
+          logLevel = 'Trace',
+          cachingLevel = 'Library',
+        },
+        formatting = {
+          provider = 'yapf'
+        },
+        venvFolders = {
+          "envs",
+          ".pyenv",
+          ".direnv",
+          ".cache/pypoetry/virtualenvs"
+        },
+        workspaceSymbols = { enabled = true }
+      }
+    },
+    root_dir = function(fname)
+      return nvim_lsp.util.root_pattern(
+        'pyproject.toml',
+        'setup.py',
+        'setup.cfg',
+        'requirements.txt',
+        'mypy.ini',
+        '.pylintrc',
+        '.flake8rc',
+        '.gitignore'
+      )(fname) or
+      nvim_lsp.util.find_git_anscestor(fname) or
+      vim.loop.os_homedir()
+    end;
   },
   rust_analyzer = {},
   sumneko_lua = {
