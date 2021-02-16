1881332c4234c0f852bc33b353418731d4489223
Wil Thomason <wil.thomason@gmail.com>
2021-02-15 20:28:05 -0500

    Use fzf-lsp for relevant handlers

--- neovim/.config/nvim/lua/config/fzf.lua
+++ neovim/.config/nvim/lua/config/fzf.lua
@@ -3,3 +3,6 @@ local map = require('config.utils').map
 -- Navigate buffers and repos
 map('n', '<c-b>', [[<cmd>History<cr>]], silent)
 map('n', '<c-a>', [[<cmd>GFiles<cr>]], silent)
+
+-- FZF LSP
+require('fzf_lsp').setup()
