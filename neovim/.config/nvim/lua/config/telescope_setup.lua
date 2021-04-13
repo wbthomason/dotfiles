5117f6d76c64baa661368e85a25ca463ff858a05
Wil Thomason <wil.thomason@gmail.com>
2021-04-13 01:36:29 -0400

    Tweak telescope buffers command arguments

--- neovim/.config/nvim/lua/config/telescope_setup.lua
+++ neovim/.config/nvim/lua/config/telescope_setup.lua
@@ -2,7 +2,7 @@ local map = require('config.utils').map
 
 local silent = {silent = true}
 -- Navigate buffers and repos
-map('n', '<c-a>', [[<cmd>Telescope buffers<cr>]], silent)
+map('n', '<c-a>', [[<cmd>Telescope buffers show_all_buffers=true sort_lastused=true<cr>]], silent)
 map('n', '<c-s>', [[<cmd>Telescope git_files<cr>]], silent)
 map('n', '<c-d>', [[<cmd>Telescope find_files<cr>]], silent)
 map('n', '<c-g>', [[<cmd>Telescope live_grep<cr>]], silent)
