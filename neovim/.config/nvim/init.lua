2b91fe805c7f549558d0485bf799413f3b463076
Wil Thomason <wil.thomason@gmail.com>
2021-04-22 17:42:19 -0400

    Fix deprecated Gstatus command

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -106,7 +106,7 @@ map('i', '<c-x><c-s>', '<esc><cmd>w<cr>a', silent)
 map('n', '<leader>w', '<cmd>w<cr>', {silent = true})
 
 -- Version control
-map('n', 'gs', '<cmd>Gstatus<cr>', silent)
+map('n', 'gs', '<cmd>Git<cr>', silent)
 
 -- Esc in the terminal
 map('t', 'jj', [[<C-\><C-n>]])
