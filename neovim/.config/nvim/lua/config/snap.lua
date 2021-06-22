e99a1062cd35985ce8750cf8efc2576e84534cc4
Wil Thomason <wil.thomason@gmail.com>
2021-06-22 00:11:23 -0400

    More telescope/snap tweaks

--- neovim/.config/nvim/lua/config/snap.lua
+++ neovim/.config/nvim/lua/config/snap.lua
@@ -10,7 +10,7 @@ snap.register.map('n', '<Leader>s', snap.create {
 
 snap.register.map('n', '<Leader><Leader>', snap.create {
   prompt = 'Files',
-  producer = snap.get('consumer.fzy')(snap.get('producer.ripgrep.file')),
+  producer = snap.get('consumer.fzy')(snap.get('producer.fd.file')),
   select = snap.get('select.file').select,
   multiselect = snap.get('select.file').multiselect,
   views = {snap.get('preview.file')}
@@ -18,8 +18,16 @@ snap.register.map('n', '<Leader><Leader>', snap.create {
 
 snap.register.map('n', '<Leader>l', snap.create {
   prompt = 'Rg',
-  producer = snap.get('producer.ripgrep.vimgrep'),
+  producer = snap.get('consumer.limit')(10000, snap.get('producer.ripgrep.vimgrep')),
   select = snap.get('select.vimgrep').select,
   multiselect = snap.get('select.vimgrep').multiselect,
   views = {snap.get('preview.vimgrep')}
+})
+
+snap.register.map('n', '<leader>b', snap.create {
+  producer = snap.get('consumer.fzy')(snap.get('consumer.combine')(snap.get('producer.vim.buffer'),
+                                                                   snap.get('producer.vim.oldfile'))),
+  select = snap.get'select.file'.select,
+  multiselect = snap.get'select.file'.multiselect,
+  views = {snap.get 'preview.file'}
 })
--- neovim/.config/nvim/lua/config/telescope.lua
+++ neovim/.config/nvim/lua/config/telescope.lua
@@ -1,3 +1,2 @@
 local telescope = require('telescope')
 telescope.setup {defaults = {layout_strategy = 'flex', scroll_strategy = 'cycle'}}
-telescope.load_extension('frecency')
