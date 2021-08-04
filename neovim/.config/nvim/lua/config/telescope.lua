2732de13b6bda33bdff6fffb8afa5015b435b607
Wil Thomason <wil.thomason@gmail.com>
2021-08-04 17:37:06 -0400

    Tweak telescope config to be nicer

--- neovim/.config/nvim/lua/config/telescope.lua
+++ neovim/.config/nvim/lua/config/telescope.lua
@@ -1,5 +1,14 @@
 local telescope = require 'telescope'
 telescope.setup {
-  defaults = { layout_strategy = 'flex', scroll_strategy = 'cycle' },
+  defaults = {
+    layout_strategy = 'flex',
+    scroll_strategy = 'cycle',
+  },
   extensions = { frecency = { workspaces = { exo = '/home/wil/projects/research/exoplanet' } } },
+  pickers = {
+    buffers = {
+      sort_lastused = true,
+      previewer = false,
+    },
+  },
 }
--- neovim/.config/nvim/lua/config/telescope_setup.lua
+++ neovim/.config/nvim/lua/config/telescope_setup.lua
@@ -1,8 +1,9 @@
 local map = require('config.utils').map
 
-local silent = {silent = true}
+local silent = { silent = true }
 -- Navigate buffers and repos
-map('n', '<c-a>', [[<cmd>Telescope buffers show_all_buffers=true<cr>]], silent)
-map('n', '<c-s>', [[<cmd>Telescope git_files<cr>]], silent)
-map('n', '<c-d>', [[<cmd>Telescope find_files<cr>]], silent)
-map('n', '<c-g>', [[<cmd>Telescope live_grep<cr>]], silent)
+map('n', '<c-a>', [[<cmd>Telescope buffers show_all_buffers=true theme=get_dropdown<cr>]], silent)
+map('n', '<c-e>', [[<cmd>Telescope frecency theme=get_dropdown<cr>]], silent)
+map('n', '<c-s>', [[<cmd>Telescope git_files theme=get_dropdown<cr>]], silent)
+map('n', '<c-d>', [[<cmd>Telescope find_files theme=get_dropdown<cr>]], silent)
+map('n', '<c-g>', [[<cmd>Telescope live_grep theme=get_dropdown<cr>]], silent)
