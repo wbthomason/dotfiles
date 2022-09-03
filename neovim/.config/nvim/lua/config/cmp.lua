3ae83d0657ebc4df9afe852c3fcaeaa1cb53be00
Wil Thomason <wil.thomason@gmail.com>
2022-09-03 18:04:04 -0500

    Use cmp in the command line again

--- neovim/.config/nvim/lua/config/cmp.lua
+++ neovim/.config/nvim/lua/config/cmp.lua
@@ -109,10 +109,10 @@ cmp.setup {
   },
 }
 
--- cmp.setup.cmdline('/', {
---   sources = cmp.config.sources({
---     { name = 'nvim_lsp_document_symbol' },
---   }, {
---     { name = 'buffer' },
---   }),
--- })
+cmp.setup.cmdline('/', {
+  sources = { { name = 'buffer' } },
+})
+
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } }),
+})
