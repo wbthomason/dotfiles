acd70c98f5ce47b160401a5626a90dc716aa5d9d
Wil Thomason <wil.thomason@gmail.com>
2022-09-03 01:01:11 -0500

    Tweak latexrun in vimtex

--- neovim/.config/nvim/plugin/vimtex.lua
+++ neovim/.config/nvim/plugin/vimtex.lua
@@ -1,5 +1,4 @@
 local g = vim.g
-g.vimtex_compiler_progname = 'nvr'
 g.vimtex_complete_recursive_bib = 1
 g.vimtex_complete_enabled = 1
 g.vimtex_quickfix_method = 'pplatex'
@@ -8,5 +7,6 @@ g.vimtex_quickfix_mode = 0
 g.vimtex_view_forward_search_on_start = 0
 g.vimtex_view_method = 'zathura'
 g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
+g.vimtex_compiler_latexrun = { options = { '-verbose-cmds', '--latex-args="-synctex=1"', '--bibtex-cmd=biber' } }
 -- This must be a dictionary, and {} gets converted to a list
 g.vimtex_syntax_conceal_disable = 1
