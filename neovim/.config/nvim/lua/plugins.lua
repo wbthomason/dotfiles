5c29d97dc7af964ac6b0c748c6de95e8bf4261c8
Wil Thomason <wil.thomason@gmail.com>
2022-09-04 02:21:43 -0500

    Trying neo-tree

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -243,6 +243,16 @@ local function init()
 
   -- Path navigation
   use 'justinmk/vim-dirvish'
+  use {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v2.x',
+    config = [[vim.g.neo_tree_remove_legacy_commands = true]],
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'kyazdani42/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+  }
 
   -- LaTeX
   use 'lervag/vimtex'
