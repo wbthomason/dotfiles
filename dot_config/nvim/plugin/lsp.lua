afa297fb9ae4a63c747ed2b6bf37281c6a3d207e
Wil Thomason <wil.thomason@gmail.com>
2022-10-20 17:28:01 -0700

    Change to API for cmp-nvim-lsp

--- dot_config/nvim/plugin/lsp.lua
+++ dot_config/nvim/plugin/lsp.lua
@@ -143,8 +143,7 @@ local servers = {
   vimls = {},
 }
 
-local client_capabilities = vim.lsp.protocol.make_client_capabilities()
-client_capabilities = require('cmp_nvim_lsp').update_capabilities(client_capabilities)
+local client_capabilities = require('cmp_nvim_lsp').default_capabilities()
 client_capabilities.offsetEncoding = { 'utf-16' }
 
 require('clangd_extensions').setup {
