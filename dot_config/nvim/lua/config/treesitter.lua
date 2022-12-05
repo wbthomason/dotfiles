2ccc9f296a9727064bf97752eb5debe48c3b6f76
Wil Thomason <wil.thomason@gmail.com>
2022-12-04 20:13:52 -0600

    And forgot to add the treesitter module

---
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -0,0 +1,29 @@
+require('nvim-treesitter.configs').setup {
+  auto_install = true,
+  highlight = { enable = true },
+  indent = { enable = false },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<cr>',
+      node_incremental = '<tab>',
+      scope_incremental = '<cr>',
+      scope_decremental = '<s-cr>',
+      node_decremental = '<s-tab>',
+    },
+  },
+  refactor = {
+    smart_rename = { enable = true, keymaps = { smart_rename = 'grr' } },
+    highlight_definitions = { enable = true },
+  },
+  textsubjects = {
+    enable = true,
+    lookahead = true,
+    keymaps = {
+      ['.'] = 'textsubjects-smart',
+      [';'] = 'textsubjects-container-outer',
+      ['i;'] = 'textsubjects-container-inner',
+    },
+  },
+  endwise = { enable = true },
+}
