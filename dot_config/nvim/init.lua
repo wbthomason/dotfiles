51bd7b9b417feade73301cb6276d18e1a8271860
Wil Thomason <wil.thomason@gmail.com>
2022-12-05 01:50:32 -0600

    Reorder modules in init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,4 @@
-require 'impatient'.enable_profile()
+--require 'impatient'.enable_profile()
 -- vim.go.packpath = '~/.local/share/nvim/site'
 vim.notify = function(_, m, l, o)
   local notify = require 'notify'
@@ -7,7 +7,7 @@ vim.notify = function(_, m, l, o)
 end
 require 'config.disable_builtins'
 require 'config.options'
-require 'config.colorscheme'
 require 'config.autocmds'
 require 'config.commands'
 require 'config.keymaps'
+require 'config.colorscheme'
