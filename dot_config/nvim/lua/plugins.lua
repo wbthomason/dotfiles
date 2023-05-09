263e6b1eb2447d40552bf23620f37174f1dd30ac
Wil Thomason <wil.thomason@gmail.com>
2023-05-09 14:25:05 -0500

    Tweak TODO highlight again

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -410,7 +410,7 @@ return {
     'folke/todo-comments.nvim',
     dependencies = 'nvim-lua/plenary.nvim',
     event = 'BufReadPost',
-    opts = { colors = { info = { 'AquaHover' } } },
+    opts = { colors = { info = { 'WhiteHover' } } },
   },
   {
     'ethanholz/nvim-lastplace',
