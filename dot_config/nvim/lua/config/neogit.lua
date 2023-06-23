dca977d61646fe6ee74344bda1e471911192291b
Wil Thomason <wil.thomason@gmail.com>
2023-06-23 16:26:10 -0500

    Tweak neogit setup to show more by default

--- dot_config/nvim/lua/config/neogit.lua
+++ dot_config/nvim/lua/config/neogit.lua
@@ -1,6 +1,29 @@
 require('neogit').setup {
   disable_commit_confirmation = true,
   disable_signs = true,
-  disable_builtin_notifications = true,
+  disable_builtin_notifications = false,
   integrations = { diffview = true },
+  sections = {
+    untracked = {
+      folded = false
+    },
+    unstaged = {
+      folded = false
+    },
+    staged = {
+      folded = false
+    },
+    stashes = {
+      folded = true
+    },
+    unpulled = {
+      folded = false
+    },
+    unmerged = {
+      folded = false
+    },
+    recent = {
+      folded = false
+    },
+  },
 }
