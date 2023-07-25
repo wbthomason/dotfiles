4df1ddccf1e9d675ddef5f91c51fc2b021f138b0
Wil Thomason <wil.thomason@gmail.com>
2023-07-25 13:31:34 -0500

    Nil check in cmp config

--- dot_config/nvim/lua/config/cmp.lua
+++ dot_config/nvim/lua/config/cmp.lua
@@ -50,7 +50,10 @@ cmp.setup {
       local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
       local strings = vim.split(kind.kind, '%s', { trimempty = true })
       kind.kind = ' ' .. strings[1] .. ' '
-      kind.menu = '    (' .. strings[2] .. ')'
+      if strings[2] ~= nil then
+        kind.menu = '    (' .. strings[2] .. ')'
+      end
+
       return kind
     end,
   },
