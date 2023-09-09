8a63cc21c53b935022dec2a529f98007eb1c477c
Wil Thomason <wil.thomason@gmail.com>
2023-09-08 21:31:46 -0500

    Still tweaking Treesitter on very large files

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -24,7 +24,17 @@ require('nvim-treesitter.configs').setup {
   },
   refactor = {
     smart_rename = { enable = true, keymaps = { smart_rename = 'grr' } },
-    highlight_definitions = { enable = true, max_file_lines = 1000 },
+    highlight_definitions = {
+      enable = true,
+      max_file_lines = 1000,
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    },
   },
   textsubjects = {
     enable = true,
