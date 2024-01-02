b929e00576645d09e5ce7829ae0a52e880fab002
Wil Thomason <wil.thomason@gmail.com>
2024-01-02 12:41:52 -0600

    Neovim colorscheme tweaks

--- dot_config/nvim/lua/config/colorscheme.lua
+++ dot_config/nvim/lua/config/colorscheme.lua
@@ -1,7 +1,10 @@
 -- Colorscheme
 vim.opt.termguicolors = true
-vim.opt.background = 'light'
+vim.opt.background = 'dark'
 
 -- cmd [[colorscheme gruvbox-material]]
 vim.cmd [[colorscheme nazgul]]
+-- vim.cmd [[colorscheme zenbones]]
+-- vim.cmd [[colorscheme substrata]]
+vim.api.nvim_set_hl(0, 'WhiteHover', { fg = '#eaeaea', bold = true })
 -- cmd [[colorscheme base16-gruvbox-light-hard]]
