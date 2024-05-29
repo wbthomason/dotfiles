fd3cc12d84094543545eed4f004f4fe04231c706
Wil Thomason <wil.thomason@gmail.com>
2024-05-29 11:09:12 -0500

    Neovim: WIP describing bindings

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,31 +1,29 @@
 -- Keybindings
-local silent = { silent = true, noremap = true }
-
 -- Quit, close buffers, etc.
-vim.keymap.set('n', '<leader>q', '<cmd>qa<cr>', silent)
-vim.keymap.set('n', '<leader>x', '<cmd>x!<cr>', silent)
-vim.keymap.set('n', '<leader>d', '<cmd>BufDel<cr>', { silent = true, nowait = true, noremap = true })
+vim.keymap.set('n', '<leader>q', '<cmd>qa<cr>', { desc = 'Quit' })
+vim.keymap.set('n', '<leader>x', '<cmd>x!<cr>', { desc = 'Save and quit' })
+vim.keymap.set('n', '<leader>d', '<cmd>BufDel<cr>', { desc = 'Delete buffer', nowait = true })
 
 -- Save buffer
-vim.keymap.set('i', '<c-s>', '<esc><cmd>w<cr>a', silent)
-vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', silent)
+vim.keymap.set('i', '<c-s>', '<esc><cmd>w<cr>a', { desc = 'Save' })
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save' })
 
 -- Version control
-vim.keymap.set('n', 'gs', '<cmd>Neogit<cr>', silent)
+vim.keymap.set('n', '<leader>g', '<cmd>Neogit<cr>', { desc = 'Neogit' })
 
 -- Yank to clipboard
-vim.keymap.set('n', 'y+', '<cmd>set opfunc=util#clipboard_yank<cr>g@', silent)
-vim.keymap.set('v', 'y+', '<cmd>set opfunc=util#clipboard_yank<cr>g@', silent)
+vim.keymap.set('n', 'y+', '<cmd>set opfunc=util#clipboard_yank<cr>g@', { desc = 'Yank to clipboard' })
+vim.keymap.set('v', 'y+', '<cmd>set opfunc=util#clipboard_yank<cr>g@', { desc = 'Yank to clipboard' })
 
 -- Window movement
-vim.keymap.set('n', '<c-h>', '<c-w>h', silent)
-vim.keymap.set('n', '<c-j>', '<c-w>j', silent)
-vim.keymap.set('n', '<c-k>', '<c-w>k', silent)
-vim.keymap.set('n', '<c-l>', '<c-w>l', silent)
+vim.keymap.set('n', '<c-h>', '<c-w>h', { desc = 'Move to window at left' })
+vim.keymap.set('n', '<c-j>', '<c-w>j', { desc = 'Move to window below' })
+vim.keymap.set('n', '<c-k>', '<c-w>k', { desc = 'Move to window above' })
+vim.keymap.set('n', '<c-l>', '<c-w>l', { desc = 'Move to window at right' })
 
 -- Tab movement
-vim.keymap.set('n', '<c-Left>', '<cmd>tabpre<cr>', silent)
-vim.keymap.set('n', '<c-Right>', '<cmd>tabnext<cr>', silent)
+vim.keymap.set('n', '<c-Left>', '<cmd>tabpre<cr>', { desc = 'Move to tab at left' })
+vim.keymap.set('n', '<c-Right>', '<cmd>tabnext<cr>', { desc = 'Move to tab at right' })
 
 -- Make relative line jumps store jumplist locations
 vim.keymap.set('n', 'k', function()
@@ -43,3 +41,9 @@ vim.keymap.set('n', 'j', function()
 
   return 'j'
 end, { expr = true, silent = true })
+
+vim.keymap.set('n', '<leader>s', function()
+  MiniSessions.write(vim.fn.fnamemodify(vim.fn.getcwd(), ':t'), {})
+end, {
+  desc = 'Write current session',
+})
