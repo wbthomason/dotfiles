f39256ee58fc4d973748070c856b243f47751d24
Wil Thomason <wil.thomason@gmail.com>
2024-06-25 19:02:13 -0500

    fix: Remove use of deprecated gitsigns config fields

--- dot_config/nvim/lua/config/gitsigns.lua
+++ dot_config/nvim/lua/config/gitsigns.lua
@@ -1,9 +1,19 @@
+vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GreenSign' })
+vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'GitSignsAdd' })
+vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'BlueSign' })
+vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'GitSignsChange' })
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'RedSign' })
+vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'GitSignsDelete' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { link = 'RedSign' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { link = 'GitSignsTopdelete' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { link = 'PurpleSign' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { link = 'GitSignsChangedelete' })
 require('gitsigns').setup {
   signs = {
-    add = { hl = 'GreenSign', text = '│', numhl = 'GitSignsAddNr' },
-    change = { hl = 'BlueSign', text = '│', numhl = 'GitSignsChangeNr' },
-    delete = { hl = 'RedSign', text = '│', numhl = 'GitSignsDeleteNr' },
-    topdelete = { hl = 'RedSign', text = '│', numhl = 'GitSignsDeleteNr' },
-    changedelete = { hl = 'PurpleSign', text = '│', numhl = 'GitSignsChangeNr' },
+    add = { text = '│' },
+    change = { text = '│' },
+    delete = { text = '│' },
+    topdelete = { text = '│' },
+    changedelete = { text = '│' },
   },
 }
